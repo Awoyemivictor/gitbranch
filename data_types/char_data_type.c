08c0017522c4c44ff418ce143cc9a43ec908053a
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-07-25 10:28:04 +0000

    Creating malloc free

---
+++ data_types/char_data_type.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Printing out characters
+
+int main()
+{
+	char c1;
+	char c2;
+
+	c1 = 'A';
+	c2 = 'a';
+
+	printf("Convert the value of c1 to character: %c\n", c1);
+	printf("Convert the value of c2 to character: %c\n", c2);
+
+	printf("\n\n");
+
+	printf("Convert the value of c1 to integer: %d\n", c1);
+	printf("Convert the value of c2 to integer: %d\n", c2);
+}
---
+++ data_types/int_data_type.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Printing victor from integer numbers
+
+int main()
+{
+	int v1, v2, v3, v4, v5, v6;
+	int v7, v8, v9, v10, v11;
+
+	v1 = 86;
+	v2 = 105;
+	v3 = 99;
+	v4 = 116;
+	v5 = 111;
+	v6 = 114;
+	v7 = 65;
+	v8 = 119;
+	v9 = 121;
+	v10 = 101;
+	v11 = 109;
+
+	printf("My name is: %c%c%c%c%c%c %c%c%c%c%c%c%c\n", v1, v2, v3, v4, v5, v6, v7, v8, v5, v9, v10, v11, v2);
+
+	return 0;
+}
